7e0bb801fa76e97face2a2a4d7d96d0cebfc0f17
oikwsat <oikwsat@gmail.com>
2018-11-17 21:37:29 +0900

    feat: metaタグを設定

--- src/pages/yokohama-cinemas-discount-calendar.js
+++ src/pages/yokohama-cinemas-discount-calendar.js
@@ -1,13 +1,29 @@
 import React from 'react'
+import Helmet from 'react-helmet'
 import { Link } from 'gatsby'
 
 import Layout from '../components/layout'
 
-const SecondPage = () => (
+const YokohamaCinemasPage = () => (
   <Layout>
+    <Helmet
+      title='横浜駅・みなとみらいエリアの映画館 割引カレンダー'
+      meta={[
+        { name: 'description',    content: '横浜駅・みなとみらいエリアの映画館の割引デーをカレンダーにまとめています。映画館・シネコンの一般料金（大人料金）は一律「1,800円」ですが，映画館・シネコンによっては特定の「日にち」や「曜日」に割引料金を設定しており、1,100円程度の割引料金で安く鑑賞することが出来ます。' },
+        { name: 'keywords',       content: '横浜,みなとみらい,映画館,シネマ,シネコン,料金,割引,カレンダー,サービスデー' },
+        { name: 'og:type',        content: 'website' },
+        { name: 'og:url',         content: 'https://rockin.work/yokohama-cinemas-discount-calendar' },
+        { name: 'og:title',       content: '横浜駅・みなとみらいエリアの映画館 割引カレンダー' },
+        { name: 'og:site_name',   content: '横浜駅・みなとみらいエリアの映画館 割引カレンダー' },
+        { name: 'og:description', content: '横浜駅・みなとみらいエリアの映画館の割引デーをカレンダーにまとめています。映画館・シネコンの一般料金（大人料金）は一律「1,800円」ですが，映画館・シネコンによっては特定の「日にち」や「曜日」に割引料金を設定しており、1,100円程度の割引料金で安く鑑賞することが出来ます。' },
+        { name: 'twitter:card',   content: 'summary' },
+      ]}
+    >
+    </Helmet>
+
     <h1>横浜・みなとみらいエリアの映画館割引デーカレンダー</h1>
     <p>横浜駅・みなとみらいエリアには映画館が3つあって，それぞれ割引デーが設けられています。<br />アカデミー賞の時期で観たい映画がたくさんあるけど，できれば安く観たい！ということで各映画館の割引デーを把握できるカレンダーを作りました。</p>
-    <iframe src="https://calendar.google.com/calendar/embed?showTitle=0&amp;showDate=0&amp;showPrint=0&amp;showTabs=0&amp;showTz=0&amp;height=600&amp;wkst=2&amp;bgcolor=%23FFFFFF&amp;src=nnvglm1cfpop7pfa57fmopdcp8%40group.calendar.google.com&amp;color=%23B1440E&amp;ctz=Asia%2FTokyo" title="Yokohama cinemas discount calendar" width="800" height="600" frameborder="0" scrolling="no"></iframe>
+    <iframe src="https://calendar.google.com/calendar/embed?showTitle=0&amp;showDate=0&amp;showPrint=0&amp;showTabs=0&amp;showTz=0&amp;height=600&amp;wkst=2&amp;bgcolor=%23FFFFFF&amp;src=nnvglm1cfpop7pfa57fmopdcp8%40group.calendar.google.com&amp;color=%23B1440E&amp;ctz=Asia%2FTokyo" title="Yokohama cinemas discount calendar" width="800" height="600" scrolling="no"></iframe>
     <p>このページを見ることでもカレンダーを確認できますが，カレンダー右下の「+Googleカレンダー」をクリックすれば自分のGoogleカレンダーに追加することができますのでぜひ使ってみてください。</p>
     <p>スマホの場合は画面に表示しきれないので直接<a target="_blank" href="https://calendar.google.com/calendar/embed?src=bm52Z2xtMWNmcG9wN3BmYTU3Zm1vcGRjcDhAZ3JvdXAuY2FsZW5kYXIuZ29vZ2xlLmNvbQ" rel="noopener noreferrer">Googleカレンダー</a>にアクセスください。</p>
 
@@ -40,4 +56,4 @@ const SecondPage = () => (
   </Layout>
 )
 
-export default SecondPage
+export default YokohamaCinemasPage
